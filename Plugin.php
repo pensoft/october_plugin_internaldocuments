7c3e23780c7f1411c284072c6903b290e8e6550c
root <root@project-dev.pensoft.local>
2023-03-10 15:45:58 +0200

    revision history

--- Plugin.php
+++ Plugin.php
@@ -2,9 +2,24 @@
 
 use Pensoft\InternalDocuments\Components\InternalRepository;
 use System\Classes\PluginBase;
+use SaurabhDhariwal\Revisionhistory\Classes\Diff as Diff;
+use System\Models\Revision as Revision;
 
 class Plugin extends PluginBase
 {
+    public function boot(){
+        /* Extetions for revision */
+        Revision::extend(function($model){
+            /* Revison can access to the login user */
+            $model->belongsTo['user'] = ['Backend\Models\User'];
+
+            /* Revision can use diff function */
+            $model->addDynamicMethod('getDiff', function() use ($model){
+                return Diff::toHTML(Diff::compare($model->old_value, $model->new_value));
+            });
+        });
+    }
+
     public function registerComponents()
     {
       return [
